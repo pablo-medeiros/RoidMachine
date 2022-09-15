b877ee24e3a54378e447f77eddf60f1ef02798d1
pablo-medeiros <pablobddev@gmail.com>
2022-09-15 17:18:10 -0300

    Initial commit

---
+++ src/main/java/bd/pablo/roidmc/machine/RoidMachine.java
@@ -0,0 +1,17 @@
+package bd.pablo.roidmc.machine;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RoidMachine extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
